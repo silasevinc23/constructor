848770ce33570931b1a1b7f18db5110e9b116091
silasevinc <silasevinc23@gmail.com>
2024-08-19 10:37:50 +0300

    Update Program.cs

--- constructor/Program.cs
+++ constructor/Program.cs
@@ -1,22 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Reflection;
-
-
-
-//Bu örnekte sizlerden bir Bebek sınıfı tanımlamanız isteniyor.
-
-//Bebek sınıfının propertyleri -> Doğum Tarihi , Ad, Soyad 
-
-//Bebek sınıfı için 2 adet constructor tanımlayınız.
-
-//1.si Default Constructor tarzında parametre almayan bir metot.
-
-//2.si Ad ve Soyad parametrelerini alarak newleme işlemi sırasında değerleri propertylere atayan alternatif constructor.
-
-//2 Constructor içinde ortak olarak bebek nesnesi oluşturulduğunda konsol ekranında bir " Ingaaaa " yazısı görülsün ve Doğum Tarihi o an olarak atansın.
-
-//Tanımlama sonrasında her iki constructor ile de birer bebek nesnesi tanımlayarak özelliklerini konsol ekranına yazdırınız
-
 class Bebek
 {
     string _ad;
